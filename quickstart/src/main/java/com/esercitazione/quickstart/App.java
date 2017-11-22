154a90ba7306c9e4403a16b60616b5a28a62c67f
m.martinelli32 <m.martinelli32@LAB1401P51.laboratori.local>
2017-11-22 12:07:57 +0100

    Initial import

---
+++ quickstart/src/main/java/com/esercitazione/quickstart/App.java
@@ -0,0 +1,11 @@
+package com.esercitazione.quickstart;
+
+import com.google.gson.Gson;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
